6b2ae37767c6319621cc1e2df3d2c3972e6f44c9
Valentin <valentin@chaplean.coop>
2018-05-15 11:30:28 +0200

    #26872 update set password mecanic

---
+++ Utility/FormErrorUtility.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Chaplean\Bundle\UserBundle\Utility;
+
+/**
+ * Class FormErrorUtility.
+ *
+ * @package   Chaplean\Bundle\UserBundle\Utility
+ * @author    Valentin - Chaplean <valentin@chaplean.coop>
+ * @copyright 2014 - 2018 Chaplean (http://www.chaplean.coop)
+ */
+class FormErrorUtility
+{
+
+}
